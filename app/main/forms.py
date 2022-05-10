024801f4e505ca3cd0f12fd0dcae4c74a7ab72eb
Cencious <user.email>
2022-05-10 15:26:24 +0300

    import datarequired from wtform-validators

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,9 @@
 from typing_extensions import Required
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
+from wtforms import StringField, TextAreaField,  SubmitField, RadioField
+from wtforms.validators import DataRequired
 
 class commentsForm(FlaskForm):
     comment = TextAreaField('Comment', validators = [Required()])
+    vote = RadioField('default field arguments', choices =[('1', 'upvote'), ('1', 'Downvote')])
+    submit = SubmitField('SUBMIT')
