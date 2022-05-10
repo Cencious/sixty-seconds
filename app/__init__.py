5dcd9e6cc8528c1d7acf98276f41208bb92922ad
Cencious <user.email>
2022-05-10 15:07:34 +0300

    create a db instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,15 @@
+from ensurepip import bootstrap
 from flask import Flask
+from config import config_options 
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
