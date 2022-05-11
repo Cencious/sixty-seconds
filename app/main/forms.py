123cc53fdf5658058d8bf593cda98c111300c791
Cencious <user.email>
2022-05-11 17:57:37 +0300

    add class for updateprofile, pitchform,upvote and down vote

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,24 +1,25 @@
-from typing_extensions import Required
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField,  SubmitField, RadioField, SelectField
+from wtforms import StringField,TextAreaField,SubmitField, SelectField, RadioField
 from wtforms.validators import DataRequired
 
-class commentsForm(FlaskForm):
-    comment = TextAreaField('Comment', validators = [DataRequired()])
-    vote = RadioField('default field arguments', choices =[('1', 'upvote'), ('1', 'Downvote')])
-    submit = SubmitField('SUBMIT')
+class CommentsForm(FlaskForm):
+    comment = TextAreaField('Comment', validators=[DataRequired()])
+    vote=RadioField('default field arguments', choices=[('1', 'UpVote'), ('1', 'DownVote')])
+    submit = SubmitField('SUBMIT')  
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about yourself.',validators = [DataRequired()])
-    submit = SubmitField('Submit')
+    bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
+    submit = SubmitField('Submit') 
 
 class PitchForm(FlaskForm):
-    category_id = SelectField('Select category', choices = [('1', 'Interview'), ('2', 'Pick Up Lines'), ('3','Promotion'), ('4', 'Product')])
+    category_id = SelectField('Select Category', choices=[('1', 'Interview'), ('2', 'Pick Up Lines'), ('3', 'Promotion'),('4','Product')])
     content = TextAreaField('YOUR PITCH')
     submit = SubmitField('Create Pitch')
 
 class UpvoteForm(FlaskForm):
-    ''' 
-    Class to create a wft form for upvoting a pitch.
+    '''
+    Class to create a wtf form for upvoting a pitch
     '''
     submit = SubmitField('Upvote')
+    
