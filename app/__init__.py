f55951ecd9eeb32c082f57099fb855d1282e064d
Cencious <user.email>
2022-05-11 18:52:39 +0300

    register blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,45 @@
-from ensurepip import bootstrap
 from flask import Flask
 from config import config_options 
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
-
+# from . import login_Manager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
+simple = SimpleMDE()
 
 
 def create_app(config_name):
     app = Flask(__name__)
+
+    # Creating the app configuration
+    app.config.from_object(config_options[config_name])  
+
+    # initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
+
+    # Registering the blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    # configure UploadSet
+    configure_uploads(app,photos)
+
+
+    return app
