fe2e09927467b84ff6b8eb58a0b887a4c7345fb9
Cencious <user.email>
2022-05-11 18:43:25 +0300

    manage servers

---
+++ manage.py
@@ -0,0 +1,21 @@
+from app import create_app, db
+from flask_script import Manager,Server
+from app.models import User, Role, Comment,Pitch
+from  flask_migrate import Migrate, MigrateCommand
+
+#creating an app instance  
+app = create_app('development')  
+
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Pitch = Pitch, Role = Role, Comment = Comment ) 
+
+if __name__ == '__main__':
+    manager.run()
