2572c51c5fcf9721f1c87a32edb91a7d2267b4c4
Cencious <user.email>
2022-05-11 18:53:00 +0300

    .

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-# from . import login_Manager
+
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 from flask_simplemde import SimpleMDE
